f4b832b0ead04e3e7a143d55dbf80fb5ce6c79a6
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-11-30 06:53:42 +0200

    Some more tests

--- code/xmem/unique_ptr.hpp
+++ code/xmem/unique_ptr.hpp
@@ -8,11 +8,11 @@
 
 namespace xmem {
 
-// trivial unique pointer class to guarantee that the destructor of the managed object
-// is called before the pointer itself is ivalidated
-// this allows complex self-referencing from the managed object
-// we need this because not all standard library implementation have this guarantee :(
-// more here: https://ibob.bg/blog/2019/11/07/dont-use-unique_ptr-for-pimpl/
+// Unique pointer class to guarantee that the destructor of the managed object
+// is called before the pointer itself is ivalidated.
+// This allows complex self-referencing from the managed object.
+// Not all standard library implementation have this guarantee :(
+// More here: https://ibob.bg/blog/2019/11/07/dont-use-unique_ptr-for-pimpl/
 template <typename T>
 class unique_ptr
 {
@@ -20,33 +20,34 @@ public:
     using pointer = T*;
 
     unique_ptr() noexcept = default;
-    unique_ptr(std::nullptr_t) noexcept {}
     explicit unique_ptr(pointer p) noexcept : m_ptr(p) {}
-    unique_ptr(unique_ptr&& other) noexcept : m_ptr(other.release()) {}
-    unique_ptr(const unique_ptr&) = delete;
-
     ~unique_ptr() { delete m_ptr; }
 
+    unique_ptr(const unique_ptr&) = delete;
+    unique_ptr& operator=(const unique_ptr&) = delete;
+
+    unique_ptr(unique_ptr&& other) noexcept : m_ptr(other.release()) {}
     unique_ptr& operator=(unique_ptr&& other) noexcept
     {
         reset(other.release());
         return *this;
     }
+
+    unique_ptr(std::nullptr_t) noexcept {}
     unique_ptr& operator=(std::nullptr_t) noexcept
     {
         reset();
         return *this;
     }
-    unique_ptr& operator=(const unique_ptr&) = delete;
 
-    pointer release()
+    pointer release() noexcept
     {
         auto old = m_ptr;
         m_ptr = nullptr;
         return old;
     }
 
-    void reset(pointer p = nullptr)
+    void reset(pointer p = nullptr) noexcept
     {
         auto old = m_ptr;
         m_ptr = p;
@@ -56,7 +57,7 @@ public:
     explicit operator bool() const noexcept { return !!m_ptr; }
 
     pointer get() const noexcept { return m_ptr; }
-    T& operator*() const { return *m_ptr; }
+    T& operator*() const noexcept { return *m_ptr; }
     T* operator->() const noexcept { return m_ptr; }
 
 private:
@@ -73,7 +74,13 @@ template <typename T>
 auto make_unique_ptr(T&& t) -> unique_ptr<typename std::remove_reference<T>::type>
 {
     using RRT = typename std::remove_reference<T>::type;
-    return  unique_ptr<RRT>(new RRT(std::forward<T>(t)));
+    return unique_ptr<RRT>(new RRT(std::forward<T>(t)));
+}
+
+template <typename T>
+unique_ptr<T> make_unique_for_overwrite()
+{
+    return unique_ptr<T>(new T);
 }
 
 }
--- test/t-unique_ptr.cpp
+++ test/t-unique_ptr.cpp
@@ -4,10 +4,56 @@
 #include <doctest/doctest.h>
 
 #include <xmem/unique_ptr.hpp>
+#include <doctest/util/lifetime_counter.hpp>
 
 #include <vector>
 
 TEST_SUITE_BEGIN("unique_ptr");
+
+struct obj : public doctest::util::lifetime_counter<obj>
+{
+    obj() = default;
+    obj(int a, std::string b) : a(a), b(b) {}
+
+    int a = 11;
+    std::string b;
+};
+
+TEST_CASE("basic")
+{
+    obj::lifetime_stats stats;
+
+    {
+        xmem::unique_ptr<obj> e;
+        CHECK_FALSE(e);
+        CHECK_FALSE(e.get());
+        CHECK_FALSE(e.release());
+        e.reset(); // should be safe
+    }
+
+    CHECK(stats.total == 0);
+
+    {
+        auto uptr = xmem::make_unique<obj>(1, "ads");
+        CHECK(uptr);
+        CHECK(uptr->a == 1);
+        CHECK(uptr->b == "ads");
+
+        auto p = uptr.get();
+
+        auto uptr2 = std::move(uptr);
+        CHECK_FALSE(uptr);
+        CHECK(uptr2);
+        CHECK(uptr2.get() == p);
+
+        CHECK(stats.d_ctr == 1);
+        CHECK(stats.living == 1);
+    }
+
+    CHECK(stats.living == 0);
+    CHECK(stats.total == 1);
+}
+
 
 TEST_CASE("make_unique_ptr")
 {
