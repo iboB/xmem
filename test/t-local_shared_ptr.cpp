00a6f6afe788c23c1012d41322d6488cfc739b49
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-02 10:55:06 +0200

    [shared_ptr] more tests

--- test/t-local_shared_ptr.cpp
+++ test/t-local_shared_ptr.cpp
@@ -33,6 +33,7 @@ TEST_CASE("basic") {
         CHECK_FALSE(e);
         CHECK_FALSE(e.get());
         CHECK(e.use_count() == 0);
+        CHECK_FALSE(e.owner());
         e.reset(); // should be safe
         e = nullptr;
         CHECK_FALSE(e);
@@ -44,6 +45,7 @@ TEST_CASE("basic") {
         sptr o(new obj(44));
         CHECK(o);
         CHECK(o.use_count() == 1);
+        CHECK(o.owner());
         CHECK(o.get());
         CHECK(o.get()->a == 44);
         CHECK(o->a == 44);
@@ -55,14 +57,55 @@ TEST_CASE("basic") {
     {
         sptr o(new obj(11));
         auto p = o.get();
+        auto oo = o.owner();
+        CHECK(oo);
         sptr mo = std::move(o);
-        CHECK(!o);
+        CHECK_FALSE(o);
         CHECK(o.get() == 0);
         CHECK(o.use_count() == 0);
+        CHECK_FALSE(o.owner());
         CHECK(mo);
         CHECK(mo.get() == p);
+        CHECK(mo.owner() == oo);
         CHECK(mo.use_count() == 1);
 
         CHECK(stats.living == 1);
+    }
+
+    CHECK(stats.total == 2);
+    CHECK(stats.living == 0);
+
+    {
+        sptr u(new obj(10));
+        CHECK(stats.living == 1);
+        sptr o(new obj(53));
+        CHECK(stats.living == 2);
+
+        CHECK(o.owner() != u.owner());
+
+        auto o2 = o;
+        CHECK(o2.get() == o.get());
+        CHECK(o2.owner() == o.owner());
+        CHECK(o2->a == 53);
+        CHECK(o.use_count() == 2);
+        auto o3 = o2;
+        CHECK(o3.get() == o.get());
+        CHECK(o3.owner() == o.owner());
+        CHECK(o3->a == 53);
+        CHECK(o.use_count() == 3);
+        CHECK(stats.living == 2);
+        o.reset();
+        CHECK_FALSE(o);
+        CHECK(o.use_count() == 0);
+        CHECK(o2.use_count() == 2);
+        CHECK(stats.living == 2);
+        o2.reset();
+        CHECK_FALSE(o2);
+        CHECK(o3->a == 53);
+        CHECK(o3.use_count() == 1);
+        CHECK(stats.living == 2);
+        o3.reset();
+        CHECK(stats.living == 1);
+        CHECK(stats.total == 4);
     }
 }
