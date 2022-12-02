55b1b398418a52de4600224e1286c505cc9d3484
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-02 09:13:57 +0200

    Common cb/ptr management for weak_ptr and shared_ptr

--- code/xmem/basic_shared_ptr.hpp
+++ code/xmem/basic_shared_ptr.hpp
@@ -7,11 +7,48 @@
 namespace xmem {
 
 namespace impl {
-template <typename CB>
-CB* owner_cast(const void* owner) {
-    return reinterpret_cast<CB*>(const_cast<void*>(owner));
-}
-}
+// used in weak_ptr as well
+template <typename CB, typename T>
+struct cb_ptr_pair {
+    cb_ptr_pair() noexcept = default;
+
+    cb_ptr_pair(std::nullptr_t) : cb(nullptr), ptr(nullptr) {}
+
+    template <typename U>
+    cb_ptr_pair(const cb_ptr_pair<CB, U>& r) noexcept
+        : cb(r.cb)
+        , ptr(r.ptr)
+    {}
+    template <typename U>
+    cb_ptr_pair& operator=(const cb_ptr_pair<CB, U>& r) noexcept {
+        cb = r.cb;
+        ptr = r.ptr;
+    }
+
+    template <typename U>
+    cb_ptr_pair(cb_ptr_pair<CB, U>&& r) noexcept : cb(r.cb), ptr(r.ptr) {
+        r.reset();
+    }
+    template <typename U>
+    cb_ptr_pair& operator=(cb_ptr_pair<CB, U>&& r) noexcept {
+        cb = r.cb;
+        ptr = r.ptr;
+        r.reset();
+    }
+
+    void reset() {
+        cb = nullptr;
+        ptr = nullptr;
+    }
+    void swap(cb_ptr_pair& r) {
+        std::swap(cb, r.cb);
+        std::swap(ptr, r.ptr);
+    }
+
+    CB* cb;
+    T* ptr;
+};
+} // namespace impl
 
 template <typename T, typename CBT>
 class basic_weak_ptr;
@@ -23,60 +60,51 @@ public:
     using control_block_type = typename CBT::base_type;
     using weak_type = basic_weak_ptr<T, CBT>;
 
-    basic_shared_ptr() noexcept : m_cb(nullptr), m_ptr(nullptr) {}
-    basic_shared_ptr(nullptr_t) noexcept : basic_shared_ptr() {};
+    basic_shared_ptr() noexcept : m(nullptr) {}
+    basic_shared_ptr(std::nullptr_t) noexcept : basic_shared_ptr() {};
 
     template <typename U>
-    basic_shared_ptr(const basic_shared_ptr<U, CBT>& other) noexcept
-        : m_cb(other.m_cb)
-        , m_ptr(other.m_ptr)
+    basic_shared_ptr(const basic_shared_ptr<U, CBT>& r) noexcept
+        : m(r.m)
     {
-        if (m_cb) m_cb->inc_strong_ref();
+        if (m.cb) m.cb->inc_strong_ref();
     }
     template <typename U>
-    basic_shared_ptr& operator=(const basic_shared_ptr<U, CBT>& other) noexcept {
-        if (m_cb) m_cb->dec_strong_ref();
-        m_cb = other.m_cb;
-        m_ptr = other.m_ptr;
-        if (m_cb) m_cb->inc_strong_ref();
+    basic_shared_ptr& operator=(const basic_shared_ptr<U, CBT>& r) noexcept {
+        if (m.cb) m.cb->dec_strong_ref();
+        m = r.m;
+        if (m.cb) m.cb->inc_strong_ref();
         return *this;
     }
 
     template <typename U>
-    basic_shared_ptr(basic_shared_ptr<U, CBT>&& other) noexcept
-        : m_cb(other.m_cb)
-        , m_ptr(other.m_ptr)
-    {
-        other.m_cb = nullptr;
-        other.m_ptr = nullptr;
-    }
+    basic_shared_ptr(basic_shared_ptr<U, CBT>&& r) noexcept
+        : m(std::move(r))
+    {}
 
     template <typename U>
-    basic_shared_ptr& operator=(basic_shared_ptr<U, CBT>&& other) noexcept {
-        if (m_cb) m_cb->dec_strong_ref();
-        m_cb = other.m_cb;
-        m_ptr = other.m_ptr;
-        other.m_cb = nullptr;
-        other.m_ptr = nullptr;
+    basic_shared_ptr& operator=(basic_shared_ptr<U, CBT>&& r) noexcept {
+        if (m.cb) m.cb->dec_strong_ref();
+        m = std::move(r.m);
         return *this;
     }
 
     template <typename U, typename D>
     basic_shared_ptr(unique_ptr<U, D>&& uptr) {
-        m_ptr = uptr.get();
-        m_cb = new typename CBT::template uptr_type<U, D>(uptr);
+        m.ptr = uptr.get();
+        m.cb = new typename CBT::template uptr_type<U, D>(uptr);
     }
 
     template <typename U, typename D>
     basic_shared_ptr& operator=(unique_ptr<U, D>&& uptr) {
-        if (m_cb) m_cb->dec_strong_ref();
-        m_ptr = uptr.get();
-        m_cb = new typename CBT::template uptr_type<U, D>(uptr);
+        if (m.cb) m.cb->dec_strong_ref();
+        m.ptr = uptr.get();
+        m.cb = new typename CBT::template uptr_type<U, D>(uptr);
         return *this;
     }
 
     template <typename D>
-    basic_shared_ptr(nullptr_t, D d) : basic_shared_ptr(unique_ptr<T, D>(nullptr, std::move(d))) {}
+    basic_shared_ptr(std::nullptr_t, D d) : basic_shared_ptr(unique_ptr<T, D>(nullptr, std::move(d))) {}
 
     template <typename U>
     explicit basic_shared_ptr(U* p) : basic_shared_ptr(unique_ptr<U>(p)) {}
@@ -85,14 +113,13 @@ public:
     basic_shared_ptr(U* p, D d) : basic_shared_ptr(unique_ptr<U, D>(p, std::move(d))) {}
 
     ~basic_shared_ptr() {
-        if (m_cb) m_cb->dec_strong_ref();
+        if (m.cb) m.cb->dec_strong_ref();
     }
 
-    void reset(nullptr_t = nullptr) noexcept {
-        if (m_cb) {
-            m_cb->dec_strong_ref();
-            m_cb = nullptr;
-            m_ptr = nullptr;
+    void reset(std::nullptr_t = nullptr) noexcept {
+        if (m.cb) {
+            m.cb->dec_strong_ref();
+            m.reset();
         }
     }
 
@@ -105,34 +132,34 @@ public:
     template <typename U, typename D, typename A>
     void reset(U* u, D d, A a);
 
-    void swap(basic_shared_ptr& other) noexcept {
-        std::swap(m_cb, other.m_cb);
-        std::swap(m_ptr, other.m_ptr);
+    void swap(basic_shared_ptr& r) noexcept {
+        m.swap(r.m);
     }
 
-    [[nodiscard]] element_type* get() const noexcept { return m_ptr; }
-    [[nodiscard]] T& operator*() const noexcept { return *m_ptr; }
-    T* operator->() const noexcept { return m_ptr; }
-    [[nodiscard]] element_type& operator[](size_t i) const noexcept {return m_ptr[i]; }
+    [[nodiscard]] element_type* get() const noexcept { return m.ptr; }
+    [[nodiscard]] T& operator*() const noexcept { return *m.ptr; }
+    T* operator->() const noexcept { return m.ptr; }
+    [[nodiscard]] element_type& operator[](size_t i) const noexcept {return m.ptr[i]; }
 
     [[nodiscard]] long use_count() const noexcept {
-        if (!m_cb) return 0;
-        return m_cb->strong_ref_count();
+        if (!m.cb) return 0;
+        return m.cb->strong_ref_count();
     }
 
-    explicit operator bool() const noexcept { return !!m_ptr; }
+    explicit operator bool() const noexcept { return !!m.ptr; }
 
-    [[nodiscard]] const void* owner() const noexcept { return m_cb; }
+    [[nodiscard]] const void* owner() const noexcept { return m.cb; }
 
     template <typename U, typename UCBT>
-    [[nodiscard]] bool owner_before(const basic_shared_ptr<U, UCBT>& other) const noexcept {
-        return m_cb < other.owner();
+    [[nodiscard]] bool owner_before(const basic_shared_ptr<U, UCBT>& r) const noexcept {
+        return m.cb < r.m.cb;
     }
 
 private:
-    control_block_type* m_cb;
-    element_type* m_ptr;
+    impl::cb_ptr_pair<control_block_type, element_type> m;
+
+    template <typename, typename> friend class basic_shared_ptr;
+    template <typename, typename> friend class basic_weak_pt;
 };
 
-
-}
+} // namespace xmem
--- code/xmem/basic_weak_ptr.hpp
+++ code/xmem/basic_weak_ptr.hpp
@@ -12,110 +12,97 @@ public:
     using element_type = std::remove_extent_t<T>;
     using control_block_type = typename CBT::base_type;
 
-    basic_weak_ptr() noexcept : m_cb(nullptr), m_ptr(nullptr) {}
+    basic_weak_ptr() noexcept : m(nullptr) {}
 
     template <typename U>
-    basic_weak_ptr(const basic_weak_ptr<U, CBT>& other) noexcept
-        : m_cb(other.m_cb)
-        , m_ptr(other.m_ptr)
+    basic_weak_ptr(const basic_weak_ptr<U, CBT>& r) noexcept
+        : m(r.m)
     {
-        if (m_cb) m_cb->inc_weak_ref();
+        if (m.cb) m.cb->inc_weak_ref();
     }
     template <typename U>
-    basic_weak_ptr& operator=(const basic_weak_ptr<U, CBT>& other) noexcept {
-        if (m_cb) m_cb->dec_weak_ref();
-        m_cb = other.m_cb;
-        m_ptr = other.m_ptr;
-        if (m_cb) m_cb->inc_weak_ref();
+    basic_weak_ptr& operator=(const basic_weak_ptr<U, CBT>& r) noexcept {
+        if (m.cb) m.cb->dec_weak_ref();
+        m = r.m;
+        if (m.cb) m.cb->inc_weak_ref();
         return *this;
     }
 
     template <typename U>
-    basic_weak_ptr(basic_weak_ptr<U, CBT>&& other) noexcept
-        : m_cb(other.m_cb)
-        , m_ptr(other.m_ptr)
-    {
-        other.m_cb = nullptr;
-        other.m_ptr = nullptr;
-    }
+    basic_weak_ptr(basic_weak_ptr<U, CBT>&& r) noexcept
+        : m(std::move(r.m))
+    {}
     template <typename U>
-    basic_weak_ptr& operator=(basic_weak_ptr<U, CBT>&& other) noexcept {
-        if (m_cb) m_cb->dec_weak_ref();
-        m_cb = other.m_cb;
-        m_ptr = other.m_ptr;
-        other.m_cb = nullptr;
-        other.m_ptr = nullptr;
+    basic_weak_ptr& operator=(basic_weak_ptr<U, CBT>&& r) noexcept {
+        if (m.cb) m.cb->dec_weak_ref();
+        m = std::move(r.m);
         return *this;
     }
 
     template <typename U>
     basic_weak_ptr(const basic_shared_ptr<U, CBT>& sptr) noexcept
-        : m_cb(impl::owner_cast<control_block_type>(sptr.owner()))
-        , m_ptr(sptr.get())
+        : m(sptr.m)
     {
-        if (m_cb) m_cb->inc_weak_ref();
+        if (m.cb) m.cb->inc_weak_ref();
     }
     template <typename U>
     basic_weak_ptr& operator=(const basic_shared_ptr<U, CBT>& sptr) noexcept {
-        if (m_cb) m_cb->dec_weak_ref();
-        m_cb = impl::owner_cast<control_block_type>(sptr.owner());
-        m_ptr = sptr.get();
-        if (m_cb) m_cb->inc_weak_ref();
+        if (m.cb) m.cb->dec_weak_ref();
+        m = sptr.m;
+        if (m.cb) m.cb->inc_weak_ref();
         return *this;
     }
 
     ~basic_weak_ptr() {
-        if (m_cb) m_cb->dec_weak_ref();
+        if (m.cb) m.cb->dec_weak_ref();
     }
 
     void reset() noexcept {
-        if (m_cb) m_cb->dec_weak_ref();
-        m_cb = nullptr;
-        m_ptr = nullptr;
+        if (m.cb) {
+            m.cb->dec_weak_ref();
+            m.reset();
+        }
     }
 
-    void swap(basic_weak_ptr& other) noexcept {
-        std::swap(m_cb, other.m_cb);
-        std::swap(m_ptr, other.m_ptr);
+    void swap(basic_weak_ptr& r) noexcept {
+        m.swap(r.m);
     }
 
     [[nodiscard]] long use_count() const noexcept {
-        if (!m_cb) return 0;
-        return m_cb->strong_ref_count();
+        if (!m.cb) return 0;
+        return m.cb->strong_ref_count();
     }
 
     [[nodiscard]] bool expired() const noexcept {
         return use_count() == 0;
     }
 
-    explicit operator bool() const noexcept { return !!m_cb; }
+    explicit operator bool() const noexcept { return !!m.cb; }
 
-    [[nodiscard]] const void* owner() const noexcept { return m_cb; }
+    [[nodiscard]] const void* owner() const noexcept { return m.cb; }
 
     template <typename U, typename UCBT>
-    [[nodiscard]] bool owner_before(const basic_weak_ptr<U, UCBT>& other) const noexcept {
-        return m_cb < other.owner();
+    [[nodiscard]] bool owner_before(const basic_weak_ptr<U, UCBT>& r) const noexcept {
+        return m.cb < r.m.cb;
     }
 
     template <typename U, typename UCBT>
-    [[nodiscard]] bool owner_before(const basic_shared_ptr<U, UCBT>& other) const noexcept {
-        return m_cb < other.owner();
+    [[nodiscard]] bool owner_before(const basic_shared_ptr<U, UCBT>& r) const noexcept {
+        return m.cb < r.m.cb;
     }
 
     [[nodiscard]] basic_shared_ptr<T, CBT> lock() const noexcept {
-        if (!m_cb) return {};
+        if (!m.cb) return {};
 
         basic_shared_ptr<T, CBT> ret;
-        if (!m_cb->inc_strong_ref_nz()) return {};
+        if (!m.cb->inc_strong_ref_nz()) return {};
 
-        ret.m_ptr = m_ptr;
-        ret.m_cb = m_cb;
+        ret.m = m;
         return ret;
     }
 
 private:
-    control_block_type* m_cb;
-    element_type* m_ptr;
+    impl::cb_ptr_pair<control_block_type, element_type> m;
 };
 
-}
+} // namespace xmem
