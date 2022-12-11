14f0e9cc2f8d72eab724aa411b720c3666967065
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-11 06:54:45 +0200

    atomic_control_block: WiP

---
+++ code/xmem/atomic_control_block.hpp
@@ -0,0 +1,48 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+
+#include <atomic>
+
+namespace xmem {
+
+class atomic_control_block_base {
+    std::atomic_uint32_t m_strong_refs = 1;
+    std::atomic_uint32_t m_weak_refs = 1;
+public:
+    virtual ~atomic_control_block_base() noexcept = default;
+
+    virtual void destroy_resource() noexcept = 0;
+    virtual void destroy_self() noexcept = 0;
+
+    void inc_strong_ref() noexcept {
+        m_strong_refs.fetch_add(1, std::memory_order_acquire);
+    }
+    void dec_strong_ref() noexcept {
+        if (m_weak_refs.fetch_sub(1, std::memory_order_release) == 1) {
+            destroy_resource();
+            dec_weak_ref();
+        }
+    }
+    uint32_t inc_strong_ref_nz() noexcept {
+        if (!m_strong_refs) return 0;
+        return ++m_strong_refs;
+    }
+
+    void inc_weak_ref() noexcept {
+        m_weak_refs.fetch_add(1, std::memory_order_acquire);
+    }
+    void dec_weak_ref() noexcept {
+        if (m_weak_refs.fetch_sub(1, std::memory_order_release) == 1) {
+            destroy_self();
+        }
+    }
+
+    long strong_ref_count() const noexcept {
+        // don't impose guarantees here
+        return long(m_strong_refs.load(std::memory_order_relaxed);
+    }
+};
+
+}
