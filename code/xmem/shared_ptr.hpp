42ff7f7b45fe5ad858a6a076582ee223e3efcbdf
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-13 13:29:54 +0200

    shared_ptr with atomic ref count

---
+++ code/xmem/shared_ptr.hpp
@@ -0,0 +1,39 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#pragma once
+#include "common_control_block.hpp"
+#include "atomic_ref_count.hpp"
+
+namespace xmem {
+
+using atomic_control_block_factory = control_block_factory<control_block_base<atomic_ref_count>>;
+
+template <typename T>
+using shared_ptr = basic_shared_ptr<atomic_control_block_factory, T>;
+
+template <typename T>
+using weak_ptr = basic_weak_ptr<atomic_control_block_factory, T>;
+
+using enable_shared_from = basic_enable_shared_from<atomic_control_block_factory>;
+
+template <typename T>
+using enable_shared_from_this = basic_enable_shared_from_this<atomic_control_block_factory, T>;
+
+template <typename T, typename... Args>
+[[nodiscard]] shared_ptr<T> make_shared(Args&&... args) {
+    return shared_ptr<T>(atomic_control_block_factory::make_resource_cb<T>(allocator<char>{}, std::forward<Args>(args)...));
+}
+
+template <typename T>
+[[nodiscard]] auto make_shared_ptr(T&& t) -> shared_ptr<std::remove_reference_t<T>> {
+    // weirdly enough msvc doesn't like this make_shared call without the explicit xmem::
+    return xmem::make_shared<std::remove_reference_t<T>>(std::forward<T>(t));
+}
+
+template <typename T>
+[[nodiscard]] shared_ptr<T> make_shared_for_overwrite() {
+    return shared_ptr<T>(atomic_control_block_factory::make_resource_cb_for_overwrite<T>(allocator<char>{}));
+}
+
+}
---
+++ test/t-shared_ptr.cpp
@@ -0,0 +1,23 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#define XMEM_TEST_NAMESPACE xmem
+#define ENABLE_XMEM_SPECIFIC_CHECKS 1
+#include <xmem/test_init.inl>
+
+#include <xmem/shared_ptr.hpp>
+#include <doctest/doctest.h>
+TEST_SUITE_BEGIN("shared_ptr");
+
+#define test_shared_ptr shared_ptr
+#define make_test_shared make_shared
+#define make_test_shared_ptr make_shared_ptr
+#define make_test_shared_for_overwrite make_shared_for_overwrite
+
+#include <xmem/test-shared_ptr-basic.inl>
+
+#define test_weak_ptr weak_ptr
+#define enable_test_shared_from enable_shared_from
+#define enable_test_shared_from_this enable_shared_from_this
+
+#include <xmem/test-weak_ptr-shared_from-basic.inl>
